86c3daa9062c8230eef83139813c43923cadeed9
loicmacken <loicmacken@gmail.com>
2024-09-20 17:11:33 +0200

    fix: parse query correctly

--- db_connection.py
+++ db_connection.py
@@ -1,4 +1,4 @@
-from sqlalchemy.engine import Engine, CursorResult
+from sqlalchemy import Engine, CursorResult, text
 
 class DbConnection():
     def __init__(self, engine: Engine) -> None:
@@ -6,4 +6,4 @@ class DbConnection():
 
     def execute_query(self, query: str) -> CursorResult:
         with self.engine.connect() as connection:
-            return connection.execute(query)
+            return connection.execute(text(query))
