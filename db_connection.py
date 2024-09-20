6307a05ae23477f1e3da8531f157d368614b0cd8
loicmacken <loicmacken@gmail.com>
2024-09-20 17:41:32 +0200

    chore: add more queries to dbconnection

--- db_connection.py
+++ db_connection.py
@@ -1,5 +1,7 @@
 from sqlalchemy import Engine, CursorResult, text
 
+from country import Country
+
 class DbConnection():
     def __init__(self, engine: Engine) -> None:
         self.engine = engine
@@ -7,3 +9,33 @@ class DbConnection():
     def execute_query(self, query: str) -> CursorResult:
         with self.engine.connect() as connection:
             return connection.execute(text(query))
+
+    def insert_countries(self, countries: list[Country]) -> list[Country]:
+        query = "INSERT INTO countries (name, code) VALUES "
+        for country in countries:
+            query += f"('{country.name}', '{country.code}'), "
+        query = query[:-2] + ";"
+        self.execute_query(query)
+        return countries
+    
+    def get_countries(self) -> list[Country]:
+        query = "SELECT * FROM countries;"
+        result = self.execute_query(query)
+        countries = []
+        for row in result:
+            countries.append(Country(row["name"], row["code"]))
+        return countries
+    
+    def get_country(self, code: str) -> Country:
+        query = f"SELECT * FROM countries WHERE code = '{code}';"
+        result = self.execute_query(query)
+        row = result.fetchone()
+        return Country(row["name"], row["code"])
+    
+    def sort_countries(self) -> list[Country]:
+        query = "SELECT * FROM countries ORDER BY name;"
+        result = self.execute_query(query)
+        countries = []
+        for row in result:
+            countries.append(Country(row["name"], row["code"]))
+        return countries
