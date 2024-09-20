b1d2b1651944a8a40aeaec2970ad8f59c1f9d6f6
loicmacken <loicmacken@gmail.com>
2024-09-20 17:41:44 +0200

    refactor: update type hint

--- country_api_connection.py
+++ country_api_connection.py
@@ -4,6 +4,6 @@ class CountryApiConnection:
     def __init__(self, api_url: str) -> None:
         self.api_url = api_url
 
-    def get_country_info(self) -> dict:
+    def get_country_info(self) -> list[dict]:
         response = requests.get(self.api_url)
         return response.json()
