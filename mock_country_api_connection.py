de53974fc2b318267fe52fcef234f08f78b0b83b
loicmacken <loicmacken@gmail.com>
2024-09-20 17:42:05 +0200

    test: update mockers

--- mock_country_api_connection.py
+++ mock_country_api_connection.py
@@ -2,22 +2,20 @@ from country_api_connection import CountryApiConnection
 
 class MockCountryApiConnection(CountryApiConnection):
     def __init__(self, api_url: str="") -> None:
-        self.mock_data = {
-            "countries": [
-                {
-                    "name": "United States",
-                    "code": "US",
-                },
-                {
-                    "name": "India",
-                    "code": "IN",
-                },
-                {
-                    "name": "China",
-                    "code": "CN",
-                }
-            ]
-        }
+        self.mock_data = [
+            {
+                "sISOCode":"US",
+                "sName":"United States"
+            },
+            {
+                "sISOCode": "IN",
+                "sName": "India",
+            },
+            {
+                "sISOCode": "CN",
+                "sName": "China"
+            }
+        ]
 
     def get_country_info(self) -> dict:
         return self.mock_data
--- mock_db_connection.py
+++ mock_db_connection.py
@@ -8,3 +8,15 @@ class MockDbConnection(DbConnection):
 
     def execute_query(self, query: str) -> CursorResult:
         return CursorResult()
+
+    def insert_countries(self, countries: list[dict]) -> list[dict]:
+        return countries
+    
+    def get_countries(self) -> list[dict]:
+        return []
+
+    def get_country(self, code: str) -> dict:
+        return None
+    
+    def sort_countries(self) -> list[dict]:
+        return []
