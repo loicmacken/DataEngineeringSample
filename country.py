2ca22b639a7a640f5a4d8e34e0f6643094a0e60e
loicmacken <loicmacken@gmail.com>
2024-09-20 17:54:40 +0200

    test: add equality function override to be able to use assertEquals in tests

--- country.py
+++ country.py
@@ -2,3 +2,8 @@ class Country():
     def __init__(self, name: str, code: str) -> None:
         self.name = name
         self.code = code
+
+    def __eq__(self, value: object) -> bool:
+        if not isinstance(value, Country):
+            return False
+        return self.name == value.name and self.code == value.code
